a8e4e0374e5b5aa7d554c9aac9ff000673a25d7b
layerl <layerlain@yandex.ru>
2023-09-03 22:47:04 +0900

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
-'use client';
-
 import Header from '@/components/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Provider } from 'react-redux';
-import { store } from '@/redux/store';
 import { ReduxProvider } from '@/redux/provider';
 
 const inter = Inter({ subsets: ['latin'] });
